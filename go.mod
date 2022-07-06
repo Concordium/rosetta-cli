807feaafc641f26c5fa891f09ff3690a6d6548e5
Michael Bisgaard Olesen <mo@concordium.com>
2022-07-06 15:34:10 +0200

    Replace base58 lib with properly designed one
    Removed caching as it didn't appear to imply a clear performance gain.

--- go.mod
+++ go.mod
@@ -17,6 +17,8 @@ require (
 	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
 )
 
-require github.com/anaskhan96/base58check v0.0.0-20181220122047-b05365d494c4
+require (
+	github.com/btcsuite/btcd/btcutil v1.1.1
+)
 
 replace github.com/coinbase/rosetta-sdk-go => github.com/Concordium/rosetta-sdk-go v0.7.11-0.20220706095914-7942ab456d4f
--- pkg/tester/data_concordium.go
+++ pkg/tester/data_concordium.go
@@ -2,11 +2,7 @@ package tester
 
 import (
 	"context"
-	"encoding/hex"
-	"log"
-	"sync"
-
-	"github.com/anaskhan96/base58check"
+	"github.com/btcsuite/btcd/btcutil/base58"
 	"github.com/coinbase/rosetta-sdk-go/syncer"
 	"github.com/coinbase/rosetta-sdk-go/types"
 )
@@ -15,15 +11,12 @@ import (
 // by rewriting all account addresses into a "normalized" form (the zero'th alias).
 // This is necessary for DataTester to do correct accounting.
 type concordiumHelper struct {
-	helper    syncer.Helper
-	cacheLock sync.RWMutex
-	cache     map[string]string
+	helper syncer.Helper
 }
 
 func newConcordiumHelper(h syncer.Helper) *concordiumHelper {
 	return &concordiumHelper{
 		helper: h,
-		cache:  make(map[string]string),
 	}
 }
 
@@ -46,46 +39,20 @@ func (h *concordiumHelper) Block(ctx context.Context, network *types.NetworkIden
 
 func (h *concordiumHelper) normalizeAddress(account *types.AccountIdentifier) {
 	if account != nil {
-		h.cacheLock.RLock()
-		a, ok := h.cache[account.Address]
-		h.cacheLock.RUnlock()
-		if !ok {
-			a = addressToAliasZero(account.Address)
-			h.cacheLock.Lock()
-			h.cache[account.Address] = a
-			h.cacheLock.Unlock()
-		}
-		account.Address = a
+		account.Address = addressToAliasZero(account.Address)
 	}
 }
 
 func addressToAliasZero(addr string) string {
-	// Lots of redundant encoding/decoding back and forth due to the design choices of the base58check lib.
-	decodedStr, err := base58check.Decode(addr)
-	if err != nil {
+	addrBytes, versionByte, err := base58.CheckDecode(addr)
+	if err != nil || len(addrBytes) != 32 {
 		// Non-Base58Check address is likely virtual (see https://github.com/Concordium/concordium-rosetta/#identifiers);
 		// return unchanged.
 		return addr
 	}
-	decodedBytes, err := hex.DecodeString(decodedStr)
-	if err != nil {
-		// Never happens as it just inverts the hex encode steps done in base58check.Decode.
-		log.Fatal(err)
-	}
-	versionBytes, addrBytes := decodedBytes[0:1], decodedBytes[1:]
-	versionStr := hex.EncodeToString(versionBytes)
-
-	// Zero out last 3 bytes.
-	if len(addrBytes) == 32 {
-		addrBytes[29] = 0
-		addrBytes[30] = 0
-		addrBytes[31] = 0
-	}
-
-	addr0, err := base58check.Encode(versionStr, hex.EncodeToString(addrBytes))
-	if err != nil {
-		// Never happens as base58check.Encode just inverts the hex encode steps above.
-		log.Fatal(err)
-	}
-	return addr0
+	// Zero out last 3 bytes and re-encode.
+	addrBytes[29] = 0
+	addrBytes[30] = 0
+	addrBytes[31] = 0
+	return base58.CheckEncode(addrBytes, versionByte)
 }
